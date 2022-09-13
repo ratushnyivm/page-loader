725a0b6d7befc5075eb926fdc5a2039691e1ed83
ratushnyyvm <ratushnyyvm@gmail.com>
2022-09-13 21:39:42 +0300

    Change: page_loader/page_loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,17 +1,21 @@
+import os
 import requests
 import re
 
 
-def convert_name(url: str) -> str:
+def generate_file_name(url: str, output: str) -> str:
     delete_scheme = re.sub(r'((.*?)//)', '', url)
-    new_url = re.sub(r'[^\dA-Za-z]', '-', delete_scheme)
-    return new_url
+    delete_extension = os.path.splitext(delete_scheme)[0]
+    new_url = re.sub(r'[^\dA-Za-z]', '-', delete_extension)
+    return new_url if output == os.getcwd() else os.path.join(output, new_url)
 
 
-def download(url, file_path=None):
+def download(url, file_path):
     r = requests.get(url)
-    return r.text
 
+    file_name = f'{generate_file_name(url, file_path)}.html'
 
-# print(download('https://ru.hexlet.io/courses'))
-print(convert_name('https://ru.hexlet.io/courses'))
+    with open(file_name, 'w', encoding="utf-8") as x:
+        x.write(r.text)
+
+    return file_name
