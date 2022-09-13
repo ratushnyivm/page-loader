91a75d773dd3d90a585e57e51a8431d369ac887d
ratushnyyvm <ratushnyyvm@gmail.com>
2022-09-13 21:40:32 +0300

    Change: page_loader/scripts/page_loader.py

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -2,11 +2,12 @@
 
 
 from page_loader.cli import parse_input
+from page_loader.page_loader import download
 
 
 def main():
     args = parse_input()
-    print(args)
+    print(download(args.url, args.output))
 
 
 if __name__ == '__main__':
