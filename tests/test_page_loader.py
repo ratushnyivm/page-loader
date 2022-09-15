2dc303facbeed98242eadb7034fa873b1ffa3eca
ratushnyyvm <ratushnyyvm@gmail.com>
2022-09-15 21:13:49 +0300

    Fix: tests/test_page_loader.py

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -10,10 +10,6 @@ FILE_NAME_1 = os.path.join(os.getcwd(), NAME)
 FILE_NAME_2 = os.path.join(os.getcwd(), DIR_PATH, NAME)
 
 
-FILE_NAME_1 = '/home/rvm/python-project-51/ru-hexlet-io-courses.html'
-FILE_NAME_2 = '/home/rvm/python-project-51/tests/ru-hexlet-io-courses.html'
-
-
 def test_generate_file_name():
     assert generate_file_name(URL, '') == FILE_NAME_1
     assert generate_file_name(URL, DIR_PATH) == FILE_NAME_2
