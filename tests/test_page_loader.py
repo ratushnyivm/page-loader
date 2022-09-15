c23c1b166f5fb2fe5d748adb15f72e1c2e6123c8
ratushnyyvm <ratushnyyvm@gmail.com>
2022-09-15 21:30:46 +0300

    Fix: tests/test_page_loader.py

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -7,9 +7,7 @@ NAME = 'ru-hexlet-io-courses.html'
 DIR_PATH = './tests'
 
 FILE_NAME_1 = os.path.join(os.getcwd(), NAME)
-FILE_NAME_2 = os.path.join(os.getcwd(), DIR_PATH, NAME)
 
 
 def test_generate_file_name():
     assert generate_file_name(URL, '') == FILE_NAME_1
-    assert generate_file_name(URL, DIR_PATH) == FILE_NAME_2
