4346e48d1655815145ee762397c991056ac18f7d
ratushnyyvm <ratushnyyvm@gmail.com>
2022-09-17 18:24:06 +0300

    Change: test_page_loader.py

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -7,7 +7,9 @@ NAME = 'ru-hexlet-io-courses.html'
 DIR_PATH = './tests'
 
 FILE_NAME_1 = os.path.join(os.getcwd(), NAME)
+FILE_NAME_2 = os.path.join(os.getcwd(), DIR_PATH[2:], NAME)
 
 
 def test_generate_file_name():
     assert generate_file_name(URL, '') == FILE_NAME_1
+    assert generate_file_name(URL, DIR_PATH) == FILE_NAME_2
