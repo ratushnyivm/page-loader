2a14e84603a71dce896cceb23726c9a966f7a108
ratushnyyvm <ratushnyyvm@gmail.com>
2022-10-13 21:59:15 +0300

    Add: test download html, image; generate name, make dir

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,33 +1,59 @@
 import os
 import tempfile
 
-import requests_mock
-from page_loader.page_loader import download, generate_file_name
+import pytest
+import requests
+from page_loader.page_loader import (
+    download_html,
+    download_image,
+    generate_file_name,
+    make_dir,
+)
 
-URL = 'https://ru.hexlet.io/courses'
-NAME = 'ru-hexlet-io-courses.html'
+URL = 'https://site.com/blog/about'
+URL_TO_IMAGE = 'https://site.com/photos/me'
+
+NAME = 'site-com-blog-about'
+NAME_TO_IMAGE = 'site-com-photos-me.jpg'
 DIR_PATH = './tests'
 
-FILE_NAME_1 = os.path.join(os.getcwd(), NAME)
-FILE_NAME_2 = os.path.join(os.getcwd(), DIR_PATH[2:], NAME)
+
+def test_generate_name():
+    name_1 = os.path.join(os.getcwd(), NAME)
+    name_2 = os.path.join(os.getcwd(), DIR_PATH[2:], NAME)
+
+    assert generate_file_name(URL, '') == name_1
+    assert generate_file_name(URL, DIR_PATH) == name_2
 
 
-def test_generate_file_name():
-    assert generate_file_name(URL, '') == FILE_NAME_1
-    assert generate_file_name(URL, DIR_PATH) == FILE_NAME_2
+@pytest.fixture
+def fake_source(requests_mock):
+    path_to_html_before = 'tests/fixtures/site-com-blog-about_files/site-com-blog-about.html' # noqa
+    path_to_image = 'tests/fixtures/site-com-blog-about_files/site-com-photos-me.jpg' # noqa
+
+    requests_mock.get(URL, text=open(path_to_html_before, 'r').read())
+    requests_mock.get(URL_TO_IMAGE, content=open(path_to_image, 'rb').read())
 
 
-def test_download():
-    with open('tests/fixtures/ru-hexlet-io-courses_test.html') as f:
-        data = f.read()
+def test_download_html(fake_source):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        path_1 = download_html(URL, tmpdirname)
+        path_2 = os.path.join(tmpdirname, f'{NAME}.html')
+        assert path_1 == path_2
 
-    with requests_mock.Mocker() as m:
-        m.get(URL, text=data)
+        with open(path_1, encoding="utf-8") as file:
+            assert file.read() == requests.get(URL).text
 
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            download(URL, tmpdirname)
 
-            with open(os.path.join(tmpdirname, NAME)) as x:
-                data_before = x.read()
+def test_make_dir(fake_source):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        path_1 = make_dir(URL, tmpdirname)
+        path_2 = os.path.join(tmpdirname, f'{NAME}_files')
+        assert path_1 == path_2
 
-    assert data_before == data
+
+def test_download_image(fake_source):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        path_1 = download_image(URL_TO_IMAGE, tmpdirname)
+        path_2 = os.path.join(tmpdirname, f'{NAME_TO_IMAGE}')
+        assert path_1 == path_2
