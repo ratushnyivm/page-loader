47e5dea5d99251af8721a136949e4e008a6f3073
ratushnyyvm <ratushnyyvm@gmail.com>
2022-10-14 20:23:26 +0300

    Add: download link and script

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -62,25 +62,50 @@ def download_image(url: str, file_path: str) -> str:
     return file_name
 
 
-def download(url: str, file_path: str) -> str:
+def download_link(url, file_path):
+    req = requests.get(url, headers=HEADERS)
+    file_name = f'{generate_file_name(url, file_path)}.css'
+    with open(file_name, 'wb') as x:
+        x.write(req.content)
+
+    return file_name
+
+
+def download_script(url, file_path):
+    req = requests.get(url, headers=HEADERS)
+    file_name = f'{generate_file_name(url, file_path)}.js'
+    with open(file_name, 'wb') as x:
+        x.write(req.content)
+
+    return file_name
+
+
+def download(url, file_path):
+    # Скачиваю html-страницу.
     path_to_html = download_html(url, file_path)
     print(f'Путь до html-файла: {path_to_html}')
 
+    # Создаю папку с ресурсами.
     path_to_dir = make_dir(url, file_path)
     print(f'Путь до папки с файлами: {path_to_dir}')
 
+    # Извлекаю относительный путь до папки.
     relative_path_to_dir = f'./{os.path.split(path_to_dir)[1]}'
     print(f'Относительный путь до папки с файлами: {relative_path_to_dir}')
 
     print('---------------------------------------')
 
+    # Сохраняю содержимое html-страницы в переменную.
     with open(path_to_html, encoding="utf-8") as f:
         html_doc = f.read()
 
+    # Создаю объект супа.
     soup = BeautifulSoup(html_doc, 'html.parser')
 
+    # Прохожу циклом по всем тегам <img>.
     for image in soup.find_all('img'):
 
+        # Проверяю соответствие доменов.
         if urlparse(url).netloc == urlparse(image.attrs.get('src')).netloc \
                 or not len(urlparse(image.attrs.get('src')).netloc):
 
@@ -88,24 +113,69 @@ def download(url: str, file_path: str) -> str:
 
             print(image)
 
+            # Создаю абсолютную ссылку.
             image_url = urljoin(url, image.attrs.get('src'))
             print(image_url)
 
+            # Скачиваю файл.
             path_to_img = download_image(image_url, path_to_dir)
             print(path_to_img)
 
+            # Извлекаю имя файла.
             name_img = os.path.split(path_to_img)[1]
             print(name_img)
 
+            # Создаю относительный путь до файла.
             relative_path_to_img = os.path.join(
                 relative_path_to_dir,
                 name_img
             )
             print(relative_path_to_img)
 
+            # Изменяю ссылку на файл.
             image['src'] = relative_path_to_img
             print(image)
 
+    for link in soup.find_all('link'):
+
+        if urlparse(url).netloc == urlparse(link.attrs.get('href')).netloc \
+                or not len(urlparse(link.attrs.get('href')).netloc):
+
+            link_url = urljoin(url, link.attrs.get('href'))
+
+            if link_url.endswith('.css'):
+                path_to_link = download_link(link_url, path_to_dir)
+                name_link = os.path.split(path_to_link)[1]
+                relative_path_to_link = os.path.join(
+                    relative_path_to_dir,
+                    name_link
+                )
+                link['href'] = relative_path_to_link
+
+            else:
+                path_to_link = download_html(link_url, path_to_dir)
+                name_link = os.path.split(path_to_link)[1]
+                relative_path_to_link = os.path.join(
+                    relative_path_to_dir,
+                    name_link
+                )
+                link['href'] = relative_path_to_link
+
+    for script in soup.find_all('script'):
+
+        if urlparse(url).netloc == urlparse(script.attrs.get('src')).netloc \
+                or not len(urlparse(script.attrs.get('src')).netloc):
+
+            script_url = urljoin(url, script.attrs.get('src'))
+            path_to_script = download_script(script_url, path_to_dir)
+            name_script = os.path.split(path_to_script)[1]
+            relative_path_to_script = os.path.join(
+                relative_path_to_dir,
+                name_script
+            )
+            script['src'] = relative_path_to_script
+
+    # Записываю измененный html.
     with open(path_to_html, 'w', encoding="utf-8") as f:
         f.write(soup.prettify())
 
