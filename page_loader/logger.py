51ac5c9c3ec485364919c76c84144e808a877807
ratushnyyvm <ratushnyyvm@gmail.com>
2022-10-20 18:01:40 +0300

    Add: config logging

---
+++ page_loader/logger.py
@@ -0,0 +1,29 @@
+import logging.config
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'consoled': {
+            'format': '%(asctime)s :: %(levelname)s :: %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        }
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'consoled'
+        },
+    },
+
+    'loggers': {
+        'PageLoader': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        }
+    }
+}
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger("PageLoader")
